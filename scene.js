2765275a55e430847cae2bad21a7340100e6d847
Jelle <ik-jelle@hotmail.nl>
2019-02-08 14:14:30 +0100

    light and ground plane

--- scene.js
+++ scene.js
@@ -13,6 +13,11 @@ var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// light
+var light = new THREE.DirectionalLight( 0xdddddd, 1 );
+light.position.set(0, 0, 1 );
+scene.add( light );
+
 camera.position.x = 2; //move right from center of scene
 camera.position.y = 1; //move up from center of scene
 camera.position.z = 5; //move camera away from center of scene
@@ -36,9 +41,15 @@ var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
 
 scene.add( skyBox );
 
+// ground
+var geometry = new THREE.BoxGeometry(1000,0,1000);
+var material = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('texture_grass.jpg')});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
 // import camera control and rotation library
 controls = new THREE.OrbitControls( camera );
-
+controls.enableKeys= true;
 
 // RENDER
 var render = function () {
