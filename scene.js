764c2a82fca71521171f81071a88abd12542d193
Jelle <ik-jelle@hotmail.nl>
2019-02-09 15:23:29 +0100

    its possbible to load objects, but not all objects work.

--- scene.js
+++ scene.js
@@ -1,4 +1,5 @@
 // Create scene
+
 var scene = new THREE.Scene();
 
 // Create camera
@@ -22,7 +23,6 @@ camera.position.x = 2; //move right from center of scene
 camera.position.y = 1; //move up from center of scene
 camera.position.z = 5; //move camera away from center of scene
 
-
 // skybox
 var directions  = ["sky_right.png", "sky_left.png", "sky_top.png", "sky_bottom.png",  "sky_front.png","sky_back.png"];
 var materialArray = [];
@@ -41,8 +41,26 @@ var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
 
 scene.add( skyBox );
 
-// ground
-var geometry = new THREE.BoxGeometry(1000,0,1000);
+// Json models
+var loader = new THREE.ObjectLoader();
+
+// add model to scene from path
+// add x y z if you want to translate it at a given vector
+function addModel(path, x= 0, y=0 ,z=0) {
+    loader.load(path, function(object) {
+
+        // then directly add the object
+        scene.add(object);
+        object.position.set(x,y,z);
+    });
+}
+
+// add models
+addModel("./models/json/tree-toon.json",10,0,0);
+addModel("./models/json/lamp.json");
+
+// ground (should be plane, not cube)
+var geometry = new THREE.BoxGeometry(2000,0,2000);
 var material = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('texture_grass.jpg')});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
